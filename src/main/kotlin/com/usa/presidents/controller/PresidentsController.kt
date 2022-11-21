812970336e14a6389fa3d8bfc339758904698a73
Mike O'Brien <mike.j.obrien@gmail.com>
2022-11-20 22:36:27 -0800

    Update homepage message

--- src/main/kotlin/com/usa/presidents/controller/PresidentsController.kt
+++ src/main/kotlin/com/usa/presidents/controller/PresidentsController.kt
@@ -31,7 +31,7 @@ class PresidentsController {
   fun getPOTUSs() = POTUSs
 
   @GetMapping("/homepage")
-  fun getHomePage() = "List of Presidents"  
+  fun getHomePage() = "Presidents App homepage"  
   
   @GetMapping("/{id}")
   fun getPOTUSById(@PathVariable id: Int): POTUS {
