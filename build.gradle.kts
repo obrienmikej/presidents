6207473f6cbc0b4a17f4d902da50a6bd49e3eaaf
Mike O'Brien <mike.j.obrien@gmail.com>
2024-01-03 15:44:05 -0800

    update version org.springframework.boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("com.gorylenko.gradle-git-properties") version "2.4.1"
 	id("io.spring.dependency-management") version "1.1.4"
-	id("org.springframework.boot") version "3.2.0"
+	id("org.springframework.boot") version "3.2.1"
 	kotlin("jvm") version "1.9.20"
 	kotlin("plugin.spring") version "1.9.20"
 }
